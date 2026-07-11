ac70ee1b25107ef20263e85c05ebbc9643f7c8ee
C.Abhinaya <abhinaya.chandru09@gmail.com>
2026-07-11 20:19:53 +0530

    Added leetcode 9:Palindrome Number in c

---
+++ palindrome-number.c
@@ -0,0 +1,16 @@
+#LeetCode #9:Palindrome Number
+
+bool isPalindrome(int x) {
+    if (x<0)
+        return false;
+    
+    long reverse=0;
+    int original=x;
+
+    while(x!=0){
+        reverse=reverse*10+(x%10);
+        x/=10;
+    }
+
+    return original==reverse;
+}
