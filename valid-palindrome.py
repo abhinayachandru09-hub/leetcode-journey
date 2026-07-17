08daf78791c5cb3d75fb288602fa4762a373cf51
C.Abhinaya <abhinaya.chandru09@gmail.com>
2026-07-17 19:52:33 +0530

    Added LeetCode valid palindrome solution

---
+++ valid-palindrome.py
@@ -0,0 +1,11 @@
+#LeetCode #125: Valid Palindrome
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        x=""
+
+        for char in s:
+            if char.isalnum():
+                x+=char.lower()
+
+        return x==x[::-1]
