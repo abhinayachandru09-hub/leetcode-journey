2e2d981758d22370ff271bffbe2abc870d21f805
C.Abhinaya <abhinaya.chandru09@gmail.com>
2026-07-18 20:56:13 +0530

    Added leetcode majority element solution

---
+++ majority-element.py
@@ -0,0 +1,16 @@
+#LeetCode #169: Majority Element
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count={}
+
+        for num in nums:
+            count[num]=count.get(num,0)+1
+
+            if count[num]>len(nums)//2:
+                return num
+
+                    
+
+
